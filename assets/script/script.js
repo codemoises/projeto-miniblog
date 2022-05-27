d2b51c78515605abdb0096b327b0bf58fe475bec
Moises Cruz <moisesbraga223@gmail.com>
2022-05-27 12:40:46 -0300

    Área de publicação

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,16 @@
 function enter() {
     window.location.href = "feedScreen.html";
 }
+
+function username() {
+    const userName = document.getElementById('username');
+    const user = document.getElementById('user');
+
+    const name = 'Marcelo Rodriguez';
+    const user1 = 'marcelinhopika123';
+
+    userName.innerHTML = `<h1>${name}</h1>`;
+    user.innerHTML = `<p>@${user1}</p>`
+    
+}
+username()
