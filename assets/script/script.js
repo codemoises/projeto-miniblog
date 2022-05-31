eb023513e3e9a128250b1d5cac92b70509e5bd58
Moises Cruz <moisesbraga223@gmail.com>
2022-05-31 17:01:35 -0300

    Feed screen

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,32 +1,3 @@
 function enter() {
     window.location.href = "feedScreen.html";
 }
-
-function username() {
-    const userName = document.getElementById('username');
-    const user = document.getElementById('user');
-
-    const name = 'Marcelo Rodriguez';
-    const user1 = 'marcelinhopika123';
-    
-
-    userName.innerHTML = `<h1>${name}</h1>`;
-    user.innerHTML = `<p>@${user1}</p>`
-}
-username()
-
-function menuView() {
-    const btnMenu = document.getElementById('btn-menu');
-    const options = document.getElementById('options');
-    
-
-    btnMenu.addEventListener('mouseenter', function ver() {
-        options.style.display = 'block';
-    });
-    btnMenu.addEventListener('click', function sair() {
-        options.style.display = 'none';
-        
-
-    });
-}
-menuView()
